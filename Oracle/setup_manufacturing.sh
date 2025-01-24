60bfefc342d3b987f4b0292fa2cf26093bcd379c
i2x <0x8084@gmail.com>
2025-01-24 12:40:51 +0700

    update qol

--- Oracle/setup_manufacturing.sh
+++ Oracle/setup_manufacturing.sh
@@ -2,10 +2,10 @@
 docker ps --format "table {{.Image}}\t{{.Status}}\t{{.Ports}}" | column -t -s $'\t'
 
 #setup
-docker cp ManufacturingDB.sql oracle-db:/tmp/ManufacturingDB.sql
-echo EXIT | docker exec -i oracle-db sqlplus / as sysdba @/tmp/ManufacturingDB.sql
+docker exec -i oracle-db sqlplus / as sysdba < ManufacturingDB.sql
+
 
 #test query 
-cat query_001.sql | docker exec -i oracle-db sqlplus / as sysdba
-cat query_002.sql | docker exec -i oracle-db sqlplus / as sysdba
-cat query_003.sql | docker exec -i oracle-db sqlplus / as sysdba
+docker exec -i oracle-db sqlplus / as sysdba < query_001.sql
+docker exec -i oracle-db sqlplus / as sysdba < query_002.sql
+docker exec -i oracle-db sqlplus / as sysdba < query_003.sql
